6bbcb2dde7d64fea0ffab16741e6abca5e7775d2
Jitender Kumar <ohyajk@gmail.com>
2022-10-15 19:40:32 +0530

    2nd big

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,54 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export const Nav = () => {
+
+    const [state, setState] = useState(false)
+
+    const toggle = () => {
+        setState(!state)
+    }
+
     return (
-        <div className='bg-[#0095E8] flex justify-between items-center px-24 h-20 '>
-            <div className=''>
-                <img className='h-20 p-1' src="./school2.png" alt="School logo" />
+        <div>
+            {/* PC NAV */}
+            <div className='bg-[#0095E8] lg:flex justify-between items-center px-24 h-20 shadow-lg drop-shadow-xl shadow-black hidden '>
+                <div className=''>
+                    <img className='h-20 p-1' src="./school2.png" alt="School logo" />
+                </div>
+                <div className='flex text-white text-[20px] font-semibold gap-4'>
+                    <Link to="/">HOME</Link>
+                    <Link to="/academics">ACADEMICS</Link>
+                    <Link to="/faculty">FACULTY</Link>
+                    <Link to="/alumni">ALUMNI</Link>
+                    <Link to="/about">ABOUT</Link>
+                    <Link to="/contact">CONTACT</Link>
+                </div>
             </div>
-            <div className='flex text-white text-[20px] font-semibold gap-4'>
-                <Link to="/">HOME</Link>
-                <Link to="/academics">ACADEMICS</Link>
-                <Link to="/faculty">FACULTY</Link>
-                <Link to="/alumni">ALUMNI</Link>
-                <Link to="/about">ABOUT</Link>
-                <Link to="/contact">CONTACT</Link>
+            {/* MOBILE NAV */}
+            <div className='bg-[#0095E8] flex justify-between items-center px-2 h-14 lg:hidden drop-shadow-xl shadow-blue'>
+                <div className=''>
+                    <img className='h-14 p-2' src="./school2.png" alt="School logo" />
+                </div>
+                <div className='flex text-white text-[20px] font-semibold gap-4'>
+                    <button onClick={toggle}>
+                        {state ? <i className="fa-solid fa-xmark fa-spin fa-lg "></i> : <i className="fa-solid fa-bars fa-shake fa-lg "></i>}
+                    </button>
+                </div>
             </div>
+            {/* Absolute Links & Magics */}
+            {state ? <div className='absolute z-50  bg-gradient-to-b from-[#0095E8] to-[#E80095] h-screen w-screen anim'>
+                <div className='relative flex flex-col items-center pt-10 w-full h-full gap-8 text-white'>
+                    <Link to="/">HOME</Link>
+                    <Link to="/academics">ACADEMICS</Link>
+                    <Link to="/faculty">FACULTY</Link>
+                    <Link to="/alumni">ALUMNI</Link>
+                    <Link to="/about">ABOUT</Link>
+                    <Link to="/contact">CONTACT</Link>
+                </div>
+            </div> : <div></div>}
 
         </div>
+
     )
 }
--- src/components/NavTop.jsx
+++ src/components/NavTop.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React from "react";
 
 export const NavTop = () => {
-    return (
-        <div className='flex justify-between items-center bg-[#E80095] px-24 h-10'>
-            <div className='flex text-white'>
-                <img className='h-8' src="./new.png" alt="label" />
-                <marquee scrollamount='3'>This is basic example of marquee</marquee>
-            </div>
-            <div className=' text-white flex items-center gap-2 '>
-                <span className='pl-2'> <i class="fa-solid fa-envelope " /> info@st.stephens.com</span>
-                <span className='pl-2'> <i class="fa-solid fa-mobile" /> +91-8666777888</span>
-                <span className='pl-2'> <i class="fa-solid fa-calendar-days" /> 12-DEC-2122</span>
-                <span className='pl-2'> <i class="fa-regular fa-hourglass-half" /> 12:30 PM</span>
-            </div>
+  return (
+    <div>
+      {/* PC VIEW */}
+      <div className="lg:flex justify-between items-center bg-[#E80095] px-24 h-10 hidden">
+        <div className="flex text-white">
+          <img className="h-8" src="./new.png" alt="label" />
+          <marquee scrollamount="3">This is basic example of marquee</marquee>
         </div>
-    )
-}
+        <div className=" text-white flex items-center gap-2 ">
+          <span className="pl-2">
+            <i class="fa-solid fa-envelope " /> info@st.stephens.com
+          </span>
+          <span className="pl-2">
+            <i class="fa-solid fa-mobile" /> +91-8666777888
+          </span>
+          <span className="pl-2">
+            <i class="fa-solid fa-calendar-days" /> 12-DEC-2122
+          </span>
+          <span className="pl-2">
+            <i class="fa-regular fa-hourglass-half" /> 12:30 PM
+          </span>
+        </div>
+      </div>
+      {/* MOBILE VIEW */}
+      <div className="flex justify-between items-center bg-[#E80095] px-2 h-10 w-[100vw] lg:hidden">
+        <div className="flex text-white">
+          <img className="h-8" src="./new.png" alt="label" />
+          <marquee scrollamount="3">This is basic example of marquee</marquee>
+        </div>
+        <div className=" text-white flex items-center gap-3 ">
+          <i class="fa-solid fa-envelope " />
+          <i class="fa-solid fa-phone-volume"></i>
+          <i class="fa-brands fa-whatsapp"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
